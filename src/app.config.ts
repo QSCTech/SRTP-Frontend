20258f9547322b45f13836278d8201ec194ba110
Breeze-aura <auraduan666@gmail.com>
2026-05-12 21:13:20 +0800

    chore: duan_jr commit

--- src/app.config.ts
+++ src/app.config.ts
@@ -1,11 +1,29 @@
 export default defineAppConfig({
   pages: [
-    'pages/index/index'
+    'pages/index/index',
+    'pages/profile/profile',
+    'pages/room-detail/room-detail'
   ],
   window: {
     backgroundTextStyle: 'light',
-    navigationBarBackgroundColor: '#fff',
-    navigationBarTitleText: 'WeChat',
-    navigationBarTextStyle: 'black'
+    navigationBarBackgroundColor: '#22B5AF',
+    navigationBarTitleText: '组队大厅',
+    navigationBarTextStyle: 'white'
+  },
+  tabBar: {
+    color: '#808080',
+    selectedColor: '#22B5AF',
+    backgroundColor: '#FFFFFF',
+    borderStyle: 'white',
+    list: [
+      {
+        pagePath: 'pages/index/index',
+        text: '组队'
+      },
+      {
+        pagePath: 'pages/profile/profile',
+        text: '我的'
+      }
+    ]
   }
 })
