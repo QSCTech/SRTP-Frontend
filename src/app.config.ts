06b7f7a60f56f34d253c24866b311c8df000e9f9
zjuwangrui <chinesewangrui@zju.edu.cn>
2026-05-16 18:43:22 +0800

    chore: add bottom tab bar

--- src/app.config.ts
+++ src/app.config.ts
@@ -10,5 +10,28 @@ export default defineAppConfig({
     navigationBarBackgroundColor: '#fff',
     navigationBarTitleText: 'WeChat',
     navigationBarTextStyle: 'black'
-  }
+  },
+  tabBar: {
+    position: 'bottom',
+    selectedColor: '#123456',
+    list: [
+      {
+        pagePath: 'pages/index/index',
+        text: '首页',
+      },
+      {
+        pagePath: 'pages/lobby/index',
+        text: ' lobby',
+      },
+      {
+        pagePath: 'pages/profile/index',
+        text: '个人资料',
+      },
+      {
+        pagePath: 'pages/room/index',
+        text: 'room',
+      }
+    ],
+  },
+  requiredBackgroundModes: ['audio']
 })
